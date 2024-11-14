40003df69b1f6a6847b5b33b111f88f6f851e4fe
megmkr <megmkrtchyan@gmail.com>
2024-11-13 16:09:30 -0800

    new menu animation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const line = document.querySelector(".line");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 navMenu.addEventListener("click", () => {
+   offScreenMenu.classList.remove("active");
+   void offScreenMenu.offsetWidth;
    navMenu.classList.toggle("active");
    offScreenMenu.classList.toggle("active");
    line.classList.toggle("active");
